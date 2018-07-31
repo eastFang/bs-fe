d1cfe9b52d19b963fe54a7567821506ec9c6a088
秉殇 <1139630194@qq.com>
2018-08-01 00:46:36 +0800

    fix webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,6 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const apiMocker = require('webpack-api-mocker')
 
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
-/*
- * We've enabled UglifyJSPlugin for you! This minifies your app
- * in order to load faster and run less javascript.
- *
- * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
- *
- */
-
 module.exports = {
 	module: {
 		rules: [{
