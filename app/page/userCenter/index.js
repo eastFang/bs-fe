b6368c536e893e14fab4c8776826c228d87749bc
秉殇 <1139630194@qq.com>
2018-08-13 00:02:08 +0800

    add userCenter

--- app/page/userCenter/index.js
+++ app/page/userCenter/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { flyUtil } from 'aliasUtil'
+import { PageCommon } from 'aliasComponent'
+import './index.scss'
 
 export default class extends React.Component {
 	constructor(props) {
@@ -22,15 +24,40 @@ export default class extends React.Component {
 		this.fetchUserFullInfo()
 	}
 
+	renderUserFullInfoDetail() {
+		const {
+			name, mobile, email,
+			homePage, avatar, gender,
+			realName, birth, country,
+			province, city,
+		} = this.state.userFullInfo
+		return (
+			<div className='detail'>
+				<p>我的信息</p>
+				<ul className='ul'>
+					<li><span className='label-span'>用户名: </span>{name}</li>
+					<li><span className='label-span'>手机号: </span>{mobile}</li>
+					<li><span className='label-span'>邮箱: </span>{email}</li>
+					<li><span className='label-span'>主页: </span>{homePage}</li>
+					<li><span className='label-span'>头像: </span>{avatar}</li>
+					<li><span className='label-span'>性别: </span>{gender}</li>
+					<li><span className='label-span'>真实姓名: </span>{realName}</li>
+					<li><span className='label-span'>生日: </span>{birth}</li>
+					<li><span className='label-span'>所在地: </span>{country}{province}{city}</li>
+				</ul>
+			</div>
+		)
+	}
+
 	render() {
 		if (!this.state.userFullInfo) {
 			return null
 		}
-		const { nickName } = this.state.userFullInfo
 
 		return (
 			<div className='center-wrap'>
-				<p>{nickName}</p>
+				<PageCommon.Ceiling />
+				{this.renderUserFullInfoDetail()}
 			</div>
 		)
 	}
