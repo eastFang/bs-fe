5ba216d14ffa0c168e9c6f9e4c74bdc3ef359f90
eastFang <1139630194@qq.com>
2018-09-11 23:32:28 +0800

    fix message

--- app/component/message/index.js
+++ app/component/message/index.js
@@ -1,38 +1,102 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import classnames from 'classnames'
 import './index.scss'
 
-const portalRoot = document.getElementById('portal')
+const messagePortal = document.getElementById('message')
 
-const render = (type, content) => {
-	const messageUI = (type) => (
-		<div className='bs-message'>
-			<i className={`iconfont icon-${type}`}></i>
-			<div className='content'>{content}</div>
-		</div>
-	)
-	ReactDOM.render(messageUI(type), portalRoot)
-	setTimeout(() => {
-		ReactDOM.unmountComponentAtNode(document.getElementById('portal'))
-	}, 2000)
+class Message extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			messageList: []
+		}
+	}
+
+	componentDidMount() {
+		this.addMessage(this.props)
+		this.reduceMessage()
+	}
+
+	componentWillReceiveProps(nextProps) {
+		this.addMessage(nextProps)
+		if (!this.timer) {
+			this.reduceMessage()
+		}
+	}
+
+	/**
+	 * 消息加1
+	 */
+	addMessage({ type, content }) {
+		const { messageList } = this.state
+		messageList.push({
+			type,
+			content
+		})
+		this.setState({
+			messageList
+		})
+	}
+
+	/**
+	 * 消息减1
+	 */
+	reduceMessage() {
+		this.timer = setInterval(() => {
+			const { messageList } = this.state
+			if (messageList.length === 0) {
+				clearInterval(this.timer)
+				this.timer = null
+			}
+			messageList.shift()
+			this.setState({
+				messageList
+			})
+		}, 2000)
+	}
+
+	renderMessageItem(type, content, index, isEnter) {
+		const className = classnames(
+			'bs-message',
+			{
+				'bs-message-enter': isEnter
+			}
+		)
+		return (
+			<div className={className} key={index}>
+				<i className={`iconfont icon-${type}`}></i>
+				<div className='content'>{content}</div>
+			</div>
+		)
+	}
+
+	render() {
+		const length = this.state.messageList.length
+		return (
+			<div className='bs-message-wrap'>
+				{
+					this.state.messageList.map((item, index) => {
+						const isEnter = index === length - 1
+						return this.renderMessageItem(item.type, item.content, index, isEnter)
+					})
+				}
+			</div>
+		)
+	}
 }
 
-class Message {
-	static success(content) {
-		render('success', content)
-	}
-
-	static error(content) {
-		render('error', content)
-	}
-
-	static warning(content) {
-		render('warning', content)
-	}
-
-	static info(content) {
-		render('info', content)
+export default {
+	success(content) {
+		return ReactDOM.render(<Message content={content} type='success'/>, messagePortal)
+	},
+	error(content) {
+		return ReactDOM.render(<Message content={content} type='error' />, messagePortal)
+	},
+	warning(content) {
+		return ReactDOM.render(<Message content={content} type='warning' />, messagePortal)
+	},
+	info(content) {
+		return ReactDOM.render(<Message content={content} type='info' />, messagePortal)
 	}
 }
-
-export default Message
